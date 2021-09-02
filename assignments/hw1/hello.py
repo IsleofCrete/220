1d2f4aa05fd338dcc40bd26927ef9cbf4d3f0488
IsleofCrete <sew1234544@yahoo.com>
2021-09-02 12:03:52 -0400

    Completed lab1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: <Sydney Wertz>
+hello.py
+
+Problem: Hello World Assignment Due 9/3/2021
+
+Certificate of Authenticity
+This assignment is entirely my own work
+"""
+
+def main():
+    print("hello, world!")
+main()

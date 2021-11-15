5ca7a7dd3bd1d4e6534131f563e1c9e133ec60e9
IsleofCrete <sew1234544@yahoo.com>
2021-11-15 11:36:25 -0500

    Updated bouncer Homework

--- assignments/hw8/bumper.py
+++ assignments/hw8/bumper.py
@@ -11,6 +11,7 @@ I certify that his assignment is entirely my own work
 from graphics import *
 from random import randint
 import time
+from math import sqrt
 
 def main():
     win = GraphWin("Bumper Circles", 350, 350)
@@ -21,136 +22,59 @@ def main():
     ball1.setFill(color1)
     ball1.draw(win)
 
-    x1 = get_random(350)
-    y1 = get_random(350)
+    x1 = get_random(30)
+    y1 = get_random(30)
 
-    ball2 = Circle(Point(125, 125), 20)
+    ball2 = Circle(Point(150, 150), 20)
     color2 = get_random_color()
     ball2.setFill(color2)
-    #ball2.draw(win)
+    ball2.draw(win)
 
-    x2 = get_random(350)
-    y2 = get_random(350)
+    x2 = get_random(30)
+    y2 = get_random(30)
 
     timer = 0.05
-    i = 0
 
     while timer < 50:
+
+        ball1.move(x1, y1)
+        ball2.move(x2, y2)
+        time.sleep(.05)
+        timer += 0.05
+
         v1 = hit_vertical(ball1, win)
         h1 = hit_horizontal(ball1, win)
+
         v2 = hit_vertical(ball2, win)
         h2 = hit_horizontal(ball2, win)
 
-        while i < 1:
-            if (hit_vertical(ball1, win) == False) and (hit_horizontal(ball1, win) == False):
-                ball1.undraw()
-                ball1.move(x1/10, y1/10)
-                ball1.draw(win)
-                ball2.undraw()
-                ball2.move(x2/10, y2/10)
-                ball2.draw(win)
-                time.sleep(.1)
-                timer += 0.05
-            else:
-                i+=1
-                break
-
         if v1 == True:
-            ball1.undraw()
-            ball1.move(-x1/10, y1/10)
-            ball1.draw(win)
-            ball2.undraw()
-            ball2.move(x2 / 10, y2 / 10)
-            ball2.draw(win)
-            time.sleep(.1)
-            while v1 == False and h1 == False:
-                ball1.undraw()
-                ball1.move(-x1/10, y1/10)
-                ball1.draw(win)
-                ball2.undraw()
-                ball2.move(x2 / 10, y2 / 10)
-                ball2.draw(win)
-                time.sleep(.1)
-                timer += 0.05
-                if v1 == True or h1 == True or v2 == True or h2 == True:
-                    break
-                break
+            x1 = -x1
+
+            timer += 0.05
 
         elif h1 == True:
-            ball1.undraw()
-            ball1.move(x1/10, -y1/10)
-            ball1.draw(win)
-            ball2.undraw()
-            ball2.move(x2 / 10, y2 / 10)
-            ball2.draw(win)
-            time.sleep(.1)
-            while v1 == False and h1 == False:
-                ball1.undraw()
-                ball1.move(x1/10, -y1/10)
-                ball1.draw(win)
-                ball2.undraw()
-                ball2.move(x2 / 10, y2 / 10)
-                ball2.draw(win)
-                time.sleep(.1)
-                timer += 0.05
-                if v1 == True or h1 == True or v2 == True or h2 == True:
-                    break
-                break
+            y1 = -y1
+
+            timer += 0.05
 
         elif v2 == True:
-            ball2.undraw()
-            ball2.move(-x2/10, y2/10)
-            ball2.draw(win)
-            ball1.undraw()
-            ball1.move(x2 / 10, y2 / 10)
-            ball1.draw(win)
-            time.sleep(.1)
-            while v2 == False and h2 == False:
-                ball2.undraw()
-                ball2.move(-x2/10, y2/10)
-                ball2.draw(win)
-                ball1.undraw()
-                ball1.move(x2 / 10, y2 / 10)
-                ball1.draw(win)
-                time.sleep(.1)
-                timer += 0.05
-                if v2 == True or h2 == True or v1 == True or h1 == True:
-                    break
-                break
+            x2 = -x2
 
-        elif hit_horizontal(ball2, win) == True:
-            ball2.undraw()
-            ball2.move(x2/10, -y2/10)
-            ball2.draw(win)
-            ball1.undraw()
-            ball1.move(x2 / 10, y2 / 10)
-            ball1.draw(win)
-            time.sleep(.1)
-            while v2 == False and h2 == False:
-                ball2.undraw()
-                ball2.move(x2/10, -y2/10)
-                ball2.draw(win)
-                ball1.undraw()
-                ball1.move(x2 / 10, y2 / 10)
-                ball1.draw(win)
-                time.sleep(.1)
-                timer += 0.05
-                if v2 == True or h2 == True or v1 == True or h1 == True:
-                    break
-                break
+            timer += 0.05
+
+        elif h2 == True:
+            y2 = -y2
+
+            timer += 0.05
 
         elif did_collide(ball1, ball2) == True:
-            ball1.undraw()
-            ball1.move(-x1 / 10, -y1 / 10)
-            ball1.draw(win)
-            ball2.undraw()
-            ball2.move(-x2 / 10, -y2 / 10)
-            ball2.draw(win)
-            time.sleep(.1)
+            x1 = -x1
+            y1 = -y1
+            x2 = -x2
+            y2 = -y2
+
             timer += 0.05
-            if (v1 == True) or (h1 == True) or (v2 == True) or (h2 == True):
-                break
-            break
 
     win.getMouse()
     win.close()
@@ -160,45 +84,47 @@ def get_random(move_amount):
     return move
 
 def did_collide(ball1, ball2):
-    pointa1 = ball1.getP1()
-    pointa2 = ball1.getP2()
-    pxa1 = pointa1.getX()
-    pya1 = pointa1.getY()
-    pxa2 = pointa2.getX()
-    pya2 = pointa2.getY()
+    rad1 = ball1.getRadius()
+    x1 = ball1.getCenter().getX()
+    y1 = ball1.getCenter().getY()
 
-    pointb1 = ball2.getP1()
-    pointb2 = ball2.getP2()
-    pxb1 = pointb1.getX()
-    pyb1 = pointb1.getY()
-    pxb2 = pointb2.getX()
-    pyb2 = pointb2.getY()
+    rad2 = ball2.getRadius()
+    x2 = ball2.getCenter().getX()
+    y2 = ball2.getCenter().getY()
 
-    if (pointa1 == pointb1) or (pointa1 == pointb2) or (pointa2 == pointb1) or (pointa2 == pointb2):
+    tot_radius = rad1 + rad2
+
+    distance = sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
+
+    if distance <= tot_radius:
         return True
     else:
         return False
 
 def hit_vertical(ball, win):
+    win_width = win.getWidth()
+
     point1 = ball.getP1()
     point2 = ball.getP2()
     px1 = point1.getX()
     py1 = point1.getY()
     px2 = point2.getX()
     py2 = point2.getY()
-    if px1 >= 350 or px1 <= 0 or px2 >= 350 or px2 <= 0:
+    if px1 >= win_width or px1 <= 0 or px2 >= win_width or px2 <= 0:
         return True
     else:
         return False
 
 def hit_horizontal(ball, win):
+    win_width = win.getWidth()
+
     point1 = ball.getP1()
     point2 = ball.getP2()
     px1 = point1.getX()
     py1 = point1.getY()
     px2 = point2.getX()
     py2 = point2.getY()
-    if py1 >= 350 or py1 <= 0 or py2 >= 350 or py2 <= 0:
+    if py1 >= win_width or py1 <= 0 or py2 >= win_width or py2 <= 0:
         return True
     else:
         return False

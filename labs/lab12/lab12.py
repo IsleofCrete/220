bc2e465ecba85357d2ea312235db13ef154805ed
IsleofCrete <sew1234544@yahoo.com>
2021-11-22 14:12:04 -0500

    Completed lab12

--- labs/lab12/lab12.py
+++ labs/lab12/lab12.py
@@ -1,11 +1,78 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Sydney Wertz>
+<lab12>.py
 """
 
-def main():
-    # add other function calls here
-    pass
+from random import randint
 
+def find_and_remove(list, value):
+    try:
+        i = list.index(value)
+        list[i] = "Sydney"
+        print(list)
+    except:
+        pass
+
+def read_data(file_name):
+    f = open(file_name, "r")
+    data = f.read()
+    data = data.split() #will split on any white space
+    print(data)
+    return data
+
+def is_in_linear(search_val, values):
+    i = 0
+    while i < len(values):
+        if values[i] == search_val:
+            print("Value found")
+            return True
+        i += 1
+    print("Value not found")
+    return False
+
+def good_input():
+    user_number = eval(input("Enter a number: "))
+    while user_number > 10 or user_number < 1:
+        user_number = eval(input("Bad number. Enter a number: "))
+    print("good input")
+    return user_number
+
+def num_digits():
+    num = eval(input("Enter a number: "))
+    while num >= 1:
+        digits = 0
+        while num > 0:
+            num //= 10
+            digits += 1
+        print(digits)
+        num = eval(input("Enter a number: "))
+
+def hi_lo_game():
+    rand_num = randint(1, 100)
+    guess = 1
+    user_input = eval(input("Enter a number between 1 and 100: "))
+
+    while (user_input != rand_num) and (guess < 7):
+        if user_input > rand_num:
+            print("Too High")
+            guess += 1
+        elif user_input < rand_num:
+            print("Too Low")
+            guess += 1
+        user_input = eval(input("Enter a number between 1 and 100: "))
+
+    if user_input == rand_num:
+        print("You won in", guess, "guesses!")
+    else:
+        print("You took too many guesses and lost. The number was", rand_num)
+
+def main():
+    #find_and_remove([1, 2, 3, 4, 5], 3)
+    #read_data("dataSorted.txt")
+    #is_in_linear(3, [1, 2, 3, 4, 5])
+    #good_input()
+    #num_digits()
+    #hi_lo_game()
+    pass
 
 main()
